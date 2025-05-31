0232e70a2bb5679021acd97725e58bcf9165909f
Chris Norton <contact@chrisnortonjr.com>
2025-05-31 00:17:24 -0400

    added ui elements withe remix icones

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="navbar bg-base-300 ">
+        <div class="navbar-start">
+            <NuxtLink to="/" class="btn btn-ghost text-xl">
+            home
+        </NuxtLink>
+        </div>
+        <div class="navbar-center"><NuxtLink to="/" class="btn btn-ghost text-3xl">
+            MCX
+        </NuxtLink></div>
+        
+        <div class="navbar-end">
+            <NuxtLink to="/" class="btn btn-accent text-xl">
+            Login
+        </NuxtLink>
+        </div>
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template >
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+  
+</template>
+<script setup>
+onMounted(() => {
+  document.documentElement.setAttribute('data-theme', 'MCX')
+})
+</script>
